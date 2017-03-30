fd7257373610f5985ecf0675c83462171e2a4ba3
ashes999 <ashes999@yahoo.com>
2017-03-29 23:51:43 -0400

    Rework story #1

--- code/source/deengames/emanstories/model/story/Story.hx
+++ code/source/deengames/emanstories/model/story/Story.hx
@@ -2,43 +2,38 @@ package deengames.emanstories.model.story;
 
 class Story
 {
+    // Internal: only for gamedev purposes
     public var summary(default, null):String;
-    public var majorEvents = new Array<String>();
-    public var plotTwistOptions = new Array<String>();
+    public var majorEvents(default, null):Array<String>;
+    public var plotTwistOptions(default, null):Array<String>;
 
     public function new()
     {
         // For now, generate a hard-coded story. For later, generate a bunch of these through JSON data,
         // pick one at random, bada-bing, bada-boom.
 
-        this.summary = "An evil antagonist captures your family person, and races towards some location. 
-            You follow, determined to save them ...";
+        this.summary = "An evil antagonist captures your parent, and races from dungeon to dungeon. You follow, determined to save them.";
 
         // Analysis: this is mostly in-game events, plus locations/people
         this.majorEvents = [
             // Turn into data objects of some sort.
             // Confront atagonist has sub-flavours, eg. he taunts you vs. he says you are growing in power
-            "Evil {antagonist} captures {friend} at {town} and runs", // intro
-            "Confront antagonist at {location 1}, who taunts", // encounter/area 1
-            "Confront antagonist at {location 2}, who appreciates", // you are growing in power ...
-            "Final confrontation at {epic location}" // he's determined to beat you
+            "Evil {antagonist} captures {person 1} at {town} and runs", // intro
+            "Confront antagonist at {location 1}",
+            "Confront antagonist at {location 2}", // you are growing in power ...
+            "Final confrontation at {location 3 (epic)}", // he tricks you into unlocking the treasure
+            "Unlock {beast}" // final battle, plus or minus variation
         ];
 
-        // Analysis: this is mostly in-game events, plus an item or person, plus boss battle
-        // Option 1: final boss is antagonist, with the generated item
-        // Option 2: final boss is generated creature
-        // Option 3: final boss is generated creature
-        // Sounds like plot twists are "configuration" for who the boss is
-        // Pick one
         this.plotTwistOptions = [
-            "Unlock ultimate weapon {item}", // for world domination
-            "Unleash ancient evil {creature}", // ancient villian back to kill you all
-            "Ancient evil {creature} rising" // face it together
+            "{person 1} sacrifices themselves for you", // as only a parent would
+            "{person 1} escapes", // battle together
+            "{person 1} passes {skill (epic)} to you" // in-game skill, eg. 1000 slashes
         ];
 
         // Now we know: we need an antagonist, friend/family member, two locations, one epic location
         // for our final battle (eg. cliff overhang, crystal forest, floating island),
-        // and either an item or a creature (depending on the plot twist). And that's our final boss.
+        // a final-boss creature, and possibly a new skill.
 
         // We also know the key events that will take place, and have a good idea of the actual
         // in-game events (eg. what the antagonist will say or do, or what will happen).
