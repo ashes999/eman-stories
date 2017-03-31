11788979ff5ce8de34df03a3015f96cb035bb2b3
ashes999 <ashes999@yahoo.com>
2017-03-30 23:41:23 -0400

    Location name and themes Data, production code, unit test

--- code/source/deengames/emanstories/data/Repository.hx
+++ code/source/deengames/emanstories/data/Repository.hx
@@ -4,10 +4,16 @@ import quasar.io.JsonParser;
 
 class Repository
 {
+    // These are all JSON objects
     public var stories(default, null):Dynamic;
+    public var locationNames(default, null):Dynamic;
+    public var locationThemes(default, null):Dynamic;
 
-    public function new(storiesJson:String):Void
+    public function new(storiesJson:String, locationJson:String):Void
     {
         this.stories = JsonParser.parse(storiesJson);
+        var locations = JsonParser.parse(locationJson);
+        this.locationNames = locations.locations;
+        this.locationThemes = locations.themes;
     }
 }
