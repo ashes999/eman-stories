87eb7f2972e00cf20ceca74385d77d0a84c8fda2
ashes999 <ashes999@yahoo.com>
2017-04-02 01:39:03 -0400

    Game ships with seeded RNG TODO: move into helper class

--- prototype/main.js
+++ prototype/main.js
@@ -2,6 +2,15 @@ Game = {
   start: function() {
     Crafty.init(960, 540);
     Crafty.enterScene("Selection");
+    Game.seedRng(1024768);
+  },
+
+  seedRng: function(seed) {
+    Game.seededRng = new Math.seedrandom(seed);
+  },
+
+  seededRandomBetween: function(a, b) {
+    return Math.floor(Game.seededRng() * (b - a)) + a;
   }
 }
 
