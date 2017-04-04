4bfd7c361ede7df47a2cb6431f84b5285ca9fc12
ashes999 <ashes999@yahoo.com>
2017-04-04 00:30:20 -0400

    Prototype: can get RNG seed

--- game-prototyping/lib/SeededRng.js
+++ game-prototyping/lib/SeededRng.js
@@ -1,15 +1,25 @@
 (function(exports) {
 
-  var rng;
+    var rng;
+    var seed;
 
-  exports.setSeed = function(seed) {
-    rng = new Math.seedrandom(seed);
-  };
+    exports.getSeed = function() {
+        return this.seed;
+    }
 
-  exports.randomBetween = function(a, b) {
-    return Math.floor(rng() * (b - a)) + a;
-  };
+    exports.setSeed = function(seed) {
+        this.seed = seed;
+        rng = new Math.seedrandom(seed);
+    };
 
-  exports.setSeed("default seed");
+    exports.randomBetween = function(a, b) {
+        return Math.floor(rng() * (b - a)) + a;
+    };
 
+    exports.random = function(array) {
+        var index = SeededRng.randomBetween(0, array.length);
+        return array[index];
+    };
+
+    exports.setSeed("default seed");
 })(this.SeededRng = {});
